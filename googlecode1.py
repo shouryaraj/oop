fa822bc11d94431863bb173918c007323e7ba5e8
shourya <shouryaraj1999@gmail.com>
2019-04-06 21:52:49 +1100

    improvement fail

--- googlecode1.py
+++ googlecode1.py
@@ -4,24 +4,22 @@
 
 def cheque_generator(n):
    N = str(n)
-   odd = False
+   obb = False
 
 
    if (n % 2 == 0):
        A = n//2
        B = n//2
    else:
-       A = n//2 + 1
-       B = n//2 + 1
-       odd = True
+       A = (n+1)//2
+       B = (n+1)//2
+       obb = True
    C = str(A)
    D = str(B)
    k = len(C) - 1
    for j in range(0, len(C)):
-       print ("jj")
-       print(k)
        if (C[k] == "4"):
-         print("hello")
+
          if k == (len(C) - 1):
              sub = 1
          else:
@@ -32,13 +30,13 @@ def cheque_generator(n):
          D = str(B)
          print(k)
 
-       print(A,B)
+
+
        if (C.find("4") == -1 and D.find("4") == -1):
-           if odd:
-               A-=1
+           if obb:
+               A+=-1
            return [str(A), str(B)]
        k = k - 1
-
 
 
 
@@ -52,4 +50,5 @@ for j in range(input_T):
     result = cheque_generator(input_N)
 
     print("Case #"+ str(j+1) +": " + result[0] +" "+ result[1])
+
 
