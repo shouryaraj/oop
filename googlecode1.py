fb83e7fc3aa8062ba668d6a7c3e676990da46037
shourya <shouryaraj1999@gmail.com>
2019-04-06 22:28:43 +1100

    New Algorithm
    101010

--- googlecode1.py
+++ googlecode1.py
@@ -4,39 +4,45 @@
 
 def cheque_generator(n):
    N = str(n)
-   obb = False
+   C = "4"
+   D= "4"
+   k = len(N) - 1
+   b = 0
+   A = n
+   B = 0
+   if(N.find("4") == -1):
+       if(N[len(N)-1] == "5"):
+           A = n - 2
+           B = 2
+           return [str(A), str(B)]
+       else:
+           A = n-1
+           B = 1
+           return [str(A), str(B)]
+   for j in range(0, len(N)):
 
 
-   if (n % 2 == 0):
-       A = n//2
-       B = n//2
-   else:
-       A = (n+1)//2
-       B = (n+1)//2
-       obb = True
-   C = str(A)
-   D = str(B)
-   k = len(C) - 1
-   for j in range(0, len(C)):
-       if (C[k] == "4"):
+       if (str(A)[k] == "4"):
 
-         if k == (len(C) - 1):
-             sub = 1
-         else:
-             sub = 10**j
-         A-=sub
-         B+=sub
-         C = str(A)
-         D = str(B)
-         print(k)
+           if k == (len(N) - 1):
+               sub = 1
+           else:
+               sub = 10 ** j
+           A -= sub
+           B += sub
+           C = str(A)
+           D = str(B)
+           print(A)
+           print(B)
 
-
+       k = k - 1
 
        if (C.find("4") == -1 and D.find("4") == -1):
-           if obb:
-               A+=-1
            return [str(A), str(B)]
-       k = k - 1
+
+
+ 
+
 
 
 
